f80820b87acd90916dbe6288f8c1e8fd94048a97
PRaymium <90473035+PRaymium@users.noreply.github.com>
2021-09-18 21:51:54 +0300

    6

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var preview_logo = document.getElementsByClassName('pre-logo');
 var preview_logo_bg = document.getElementsByClassName('pre-logo-container');
 
-var contact_email = document.getElementsByClassName('email');
-var contact_phone = document.getElementsByClassName('contact-phone');
-
 window.onload = function()
 {
     preview_logo[0].classList.add('active');
     preview_logo_bg[0].classList.add('active');
 }
 
-contact_email[0].onclick = function()
+function phoneCopy()
 {
-    contact_email[0].select();
-    document.execCommand('copy');
+    navigator.clipboard.writeText("+7 920 243 90 97");
 }
+
+function emailCopy()
+{
+    navigator.clipboard.writeText("Jokersli@mail.ru");
+}
+
